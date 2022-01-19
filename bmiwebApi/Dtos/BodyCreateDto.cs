983ea725faad4b8ca0c435b46efa0fa4af1dbaa8
Krzysztof <Krzysztofbojarczuk@wp.pl>
2022-01-19 09:47:38 +0100

    Add project files.

---
+++ bmiwebApi/Dtos/BodyCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace bmiwebApi.Dtos
+{
+    public class BodyCreateDto
+    {
+        public double Weight { get; set; }
+        public double Height { get; set; }
+    }
+}
---
+++ bmiwebApi/Repositories/IBodyRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace bmiwebApi.Repositories
+{
+    public interface IBodyRepository
+    {
+        Task<List<Body>> GetAllBodyAsync();
+        Task<Body> GetBodyByIdAsync(int id);
+        Task<Body> CreateBodyAsync(Body body);
+        Task<Body> UpdateBodyAsync(Body updatebody);
+        Task<Body> DeleteBodyAsync(int id);
+
+        //Calc Bmi
+    }
+}
